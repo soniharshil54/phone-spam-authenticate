32f1e096990a9f1f69bda3e4c040cf015bc65a07
soniharshil54 <sharshil43@yahoo.com>
2024-04-28 14:46:07 +0530

    Add bcryptjs dependency and encrypt user password

--- api-service/src/services/auth.js
+++ api-service/src/services/auth.js
@@ -1,10 +1,19 @@
+const bcrypt = require('bcryptjs');
+
 const User = require('../db/models/User');
 
 module.exports = {
   async createUser(user) {
-    console.log('Creating user:', user);
-    const newUserRecord = await User.create(user);
-    console.log('User created:', newUserRecord);
+    const userData = {
+      ...user,
+      password: await this.encryptPassword(user.password)
+    }
+    const newUserRecord = await User.create(userData);
     return newUserRecord;
   },
+
+  async encryptPassword(password) {
+    const encryptedPassword = await bcrypt.hash(password, 10);
+    return encryptedPassword
+  }
 }
