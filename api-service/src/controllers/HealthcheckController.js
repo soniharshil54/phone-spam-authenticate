ffa540ca11d26d37dd6c1da68986c6a96055783f
soniharshil54 <sharshil43@yahoo.com>
2024-11-16 14:22:05 +0530

    v1.2.42

--- api-service/src/controllers/HealthcheckController.js
+++ api-service/src/controllers/HealthcheckController.js
@@ -2,7 +2,7 @@ module.exports = {
     healthcheck: (req, res) => {
         res.ok({
             status: 'ok 6',
-            version: 'v1.2.41'
+            version: 'v1.2.42'
         });
     }
 }
